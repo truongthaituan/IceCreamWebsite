971b11ad700984d1ee1f1e603dae87bb7aaccfa5
16110249_Trương Thái Tuấn <44605088+truongthaituan@users.noreply.github.com>
2020-04-13 15:20:06 +0700

    updated java

--- IceCreamWebsite/src/main/java/com/example/demo/repositories/faqRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repositories;
-
-import com.example.demo.models.Faq;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FaqRepository extends JpaRepository<Faq, Long> {
-}
--- IceCreamWebsite/src/main/java/com/example/demo/services/MailService.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.services;
-
-public interface MailService {
-    public void send(String toAddress, String fromAddress, String subject, String content) throws Exception;
-    public void sendHTML(String toAddress, String fromAddress, String subject, String content) throws Exception;
-
-}
